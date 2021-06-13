2155be249f6b3ab3f846f0dda216acca35f8480e
faiz <razafaiz815@gmail.com>
2021-06-14 01:14:31 +0500

    Home page

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,8 +1,34 @@
 import React from "react";
+import { Typography } from "@material-ui/core";
 import "./Home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return (
+    <div id="home" className="home">
+      <div>
+        <div className="home_left">
+          <Typography variant="subtitle1" className="typo1">
+            Welcome to my world
+          </Typography>
+          <Typography variant="h3" className="typo2">
+            Hi, I'm Faiz Shahnawaz
+          </Typography>
+          <Typography variant="h3" className="typo3">
+            JS Developer
+          </Typography>
+          <Typography variant="h3" className="typo2">
+            based in Pakistan.
+          </Typography>
+        </div>
+        <div className="home_right">
+          <img
+            src="/Images/Faiz-front-photo-removebg-preview (1).png"
+            alt="faiz-shahnawaz"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,8 +2,10 @@ import { Button, Link } from "@material-ui/core";
 import React from "react";
 import Drawer from "./drawer";
 import "./Navbar.css";
-import FacebookIcon from "@material-ui/icons/Facebook";
 import { RiFacebookFill } from "react-icons/ri";
+import { RiLinkedinFill } from "react-icons/ri";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { RiTwitterFill } from "react-icons/ri";
 
 function Navbar() {
   const preventDefault = (event) => event.preventDefault();
@@ -13,32 +15,31 @@ function Navbar() {
     <div className="navbar">
       <div className="links">
         {window.innerWidth > 800 ? (
-        <span>
-          <a href="#home" onClick={preventDefault}>
-            Home
-          </a>
-          <a href="#" onClick={preventDefault}>
-            About
-          </a>{" "}
-          <a href="#" onClick={preventDefault}>
-            Service
-          </a>{" "}
-          <a href="#" onClick={preventDefault}>
-            Portfolio
-          </a>{" "}
-          <a href="#" onClick={preventDefault}>
-            Contact
-          </a>
-        </span>
+          <span>
+            <a href="#home">Home</a>
+            <a href="#About">About</a> <a href="#Service">Service</a>{" "}
+            <a href="#Portfolio">Portfolio</a> <a href="#Contact">Contact</a>
+          </span>
         ) : (
           false
         )}
       </div>
       <div className="social_links">
-        <RiFacebookFill className="social_icon" />
-        <RiFacebookFill className="social_icon" />
-        <RiFacebookFill className="social_icon" />
-        <RiFacebookFill className="social_icon" />
+        <a href="https://www.facebook.com/M.faiz.raza.21" target="_blank">
+          <RiFacebookFill className="social_icon" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/faiz-raza-69835a166/1"
+          target="_blank"
+        >
+          <RiLinkedinFill className="social_icon" />
+        </a>
+        <a href="https://www.instagram.com/m_faiz_shahnawaz/" target="_blank">
+          <AiOutlineInstagram className="social_icon" />
+        </a>
+        <a href="https://twitter.com/FaizShahnawaz1?s=09" target="_blank">
+          <RiTwitterFill className="social_icon" />
+        </a>
       </div>
       {window.innerWidth > 800 ? (
         <Button variant="outlined" className="nav_contact_button">
