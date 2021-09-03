13f5d0545bdce763a16d9a2894bf5ef1b5b5cdee
faiz <razafaiz815@gmail.com>
2021-09-03 19:58:56 +0500

    projects updated

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -6,21 +6,27 @@ import Slide from "react-reveal/Slide";
 
 const data = [
   {
+    title: "SaaS App UI",
+    desc: "A small clone of the Amazon website with add-to-cart functionality. The cart is functional as well where you will see the selected products and total bill.",
+    img: "/Images/saas_ui.png",
+    link: "https://saas-app-ui.netlify.app/",
+  },
+  {
     title: "YouRead Blog Website",
     desc: "A blog website created with Gatsby, Contentful, Netlify with CI/CD pipeline and gated content.",
-    img: "/Images/app-bsck5.png",
+    img: "/Images/YouRead.png",
     link: "https://youread.netlify.app/",
   },
   {
     title: "The Smart Solutions",
     desc: " A website for an umbrella company that works in different tech fields and provides solutions for day to day problems.",
-    img: "/Images/app-back3.png",
+    img: "/Images/smart-solutions.png",
     link: "https://www.the-smart-solutions.com/",
   },
   {
     title: "Portfolio Website",
     desc: "A perfect website for your portfolio to showcase your potential to the recruiters in a professional and efficient way.    ",
-    img: "/Images/Screenshot from 2021-07-19 15-27-28.png",
+    img: "/Images/Portfolio.png",
     link: "https://portfolio-app-faiz.netlify.app/",
   },
   {
@@ -28,13 +34,6 @@ const data = [
     desc: " A classic COVID-19 Tracker application with ReactJS, Material-UI,CSS, Chart.js, leaflet and hosted on Firebase.",
     img: "/Images/app-back2.png",
     link: "https://covid-19-tracker-45826.web.app/",
-  },
-
-  {
-    title: "Amazon Clone",
-    desc: "A small clone of the Amazon website with add-to-cart functionality. The cart is functional as well where you will see the selected products and total bill.",
-    img: "/Images/app-back.png",
-    link: "https://clone-5145b.web.app/",
   },
 ];
 
